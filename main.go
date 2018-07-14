3a5c5a19986685d6bc2e3472765903a5fc91a282
sky <sskydragon@users.noreply.github.com>
2018-07-15 02:56:36 +0800

    fix
    -

--- main.go
+++ main.go
@@ -494,7 +494,7 @@ var u=function(){function b(a){var b=a&7,c=0,d=0;1==b||4==b?c=d=1:2==b&&(c=d=2);
 			}
 		case (strings.Contains(msg,"拔牙怪")):
 			replyMsg = "聽說拔牙怪是大魔王, 大家看到要趕緊逃命喔喵~~~~~"
-		case (strings.Contains(msg,"!偶數理論") || strings.Contains(msg,"!奇偶性"))
+		case (strings.Contains(msg,"!偶數理論") || strings.Contains(msg,"!奇偶性")):
 			replyMsg = "!偶數理論"
 		default:
 	}
